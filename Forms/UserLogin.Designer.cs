3a0e255b2363cc4297931dd5914b383089105196
jeff-s-cope <sgl879@mocs.utc.edu>
2024-03-19 16:10:12 -0500

    #

---
+++ Forms/UserLogin.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace EasyBizPos.Forms
+{
+    partial class UserLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMdiParent = new System.Windows.Forms.Panel();
+            this.RegisterBtn = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelMainLogo = new System.Windows.Forms.Label();
+            this.mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UserIdLabel = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.panelMdiParent.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMdiParent
+            // 
+            this.panelMdiParent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panelMdiParent.Controls.Add(this.LoginBtn);
+            this.panelMdiParent.Controls.Add(this.PasswordTextBox);
+            this.panelMdiParent.Controls.Add(this.UsernameTextBox);
+            this.panelMdiParent.Controls.Add(this.PasswordLabel);
+            this.panelMdiParent.Controls.Add(this.UserIdLabel);
+            this.panelMdiParent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMdiParent.Location = new System.Drawing.Point(0, 130);
+            this.panelMdiParent.Name = "panelMdiParent";
+            this.panelMdiParent.Size = new System.Drawing.Size(1127, 568);
+            this.panelMdiParent.TabIndex = 26;
+            // 
+            // RegisterBtn
+            // 
+            this.RegisterBtn.BackColor = System.Drawing.Color.MidnightBlue;
+            this.RegisterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.RegisterBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RegisterBtn.Location = new System.Drawing.Point(352, 343);
+            this.RegisterBtn.Name = "RegisterBtn";
+            this.RegisterBtn.Size = new System.Drawing.Size(0, 0);
+            this.RegisterBtn.TabIndex = 23;
+            this.RegisterBtn.Text = "Register";
+            this.RegisterBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panel3.Controls.Add(this.labelMainLogo);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1127, 130);
+            this.panel3.TabIndex = 25;
+            // 
+            // labelMainLogo
+            // 
+            this.labelMainLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.labelMainLogo.AutoSize = true;
+            this.labelMainLogo.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMainLogo.ForeColor = System.Drawing.Color.White;
+            this.labelMainLogo.Location = new System.Drawing.Point(436, 35);
+            this.labelMainLogo.Name = "labelMainLogo";
+            this.labelMainLogo.Size = new System.Drawing.Size(223, 54);
+            this.labelMainLogo.TabIndex = 0;
+            this.labelMainLogo.Text = "User Login";
+            // 
+            // mySqlCommand1
+            // 
+            this.mySqlCommand1.CacheAge = 0;
+            this.mySqlCommand1.Connection = null;
+            this.mySqlCommand1.EnableCaching = false;
+            this.mySqlCommand1.Transaction = null;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.PasswordLabel.Location = new System.Drawing.Point(356, 267);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(160, 45);
+            this.PasswordLabel.TabIndex = 6;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // UserIdLabel
+            // 
+            this.UserIdLabel.AutoSize = true;
+            this.UserIdLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserIdLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.UserIdLabel.Location = new System.Drawing.Point(356, 155);
+            this.UserIdLabel.Name = "UserIdLabel";
+            this.UserIdLabel.Size = new System.Drawing.Size(131, 45);
+            this.UserIdLabel.TabIndex = 4;
+            this.UserIdLabel.Text = "User ID:";
+            this.UserIdLabel.Click += new System.EventHandler(this.AddName_Click);
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameTextBox.Location = new System.Drawing.Point(592, 165);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(159, 35);
+            this.UsernameTextBox.TabIndex = 7;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(592, 267);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(159, 35);
+            this.PasswordTextBox.TabIndex = 8;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(503, 408);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(101, 49);
+            this.LoginBtn.TabIndex = 9;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // UserLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 698);
+            this.Controls.Add(this.panelMdiParent);
+            this.Controls.Add(this.RegisterBtn);
+            this.Controls.Add(this.panel3);
+            this.Name = "UserLogin";
+            this.Text = "Form1";
+            this.panelMdiParent.ResumeLayout(false);
+            this.panelMdiParent.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMdiParent;
+        private System.Windows.Forms.Button RegisterBtn;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelMainLogo;
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label UserIdLabel;
+        private System.Windows.Forms.MaskedTextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
---
+++ Forms/UserLogin.cs
@@ -0,0 +1,125 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyBizPos.Forms
+{
+    public partial class UserLogin : Form
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            // Validate username and password
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both username and password.");
+                return;
+            }
+
+            // Hash the password
+            string hashedPassword = HashPassword(password);
+      
+            // Check if the username and hashed password exist in the database
+            bool isAuthenticated = CheckCredentials(username, hashedPassword);
+
+            if (isAuthenticated)
+            {
+                // Allow access to the main form
+                OpenMainForm();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.");
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            // Use a secure hashing algorithm like SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        private bool CheckCredentials(string username, string password)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+
+            // Query to check if the username exists in the database
+            string userQuery = "SELECT COUNT(*) FROM employeeinfo WHERE employee_ID = @username";
+
+            // Query to get the hashed password for the username
+            string passwordQuery = "SELECT Password FROM employeeinfo WHERE employee_ID = @username";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                // Check if the username exists
+                using (MySqlCommand userCommand = new MySqlCommand(userQuery, connection))
+                {
+                    userCommand.Parameters.AddWithValue("@username", username);
+                    connection.Open();
+                    int userCount = Convert.ToInt32(userCommand.ExecuteScalar());
+
+                    if (userCount == 0)
+                    {
+                        // Username doesn't exist
+                        return false;
+                    }
+                }
+
+                // Get the hashed password for the username
+                using (MySqlCommand passwordCommand = new MySqlCommand(passwordQuery, connection))
+                {
+                    passwordCommand.Parameters.AddWithValue("@username", username);
+
+                    string hashedPassword = passwordCommand.ExecuteScalar()?.ToString();
+
+                    if (hashedPassword == null)
+                    {
+                        // Unable to retrieve hashed password
+                        return false;
+                    }
+
+                    // Compare the hashed password with the input password
+                    return VerifyHashedPassword(password, hashedPassword);
+                }
+            }
+        }
+
+        private bool VerifyHashedPassword(string password, string hashedPassword)
+        {
+            // Compare the hashed input password with the stored hashed password
+            return password == hashedPassword;
+        }
+
+        
+        private void OpenMainForm()
+        {
+            this.Hide(); // Hide the login form
+            HomeFormMain mainForm = new HomeFormMain();
+            mainForm.ShowDialog(); // Show the main form as a dialog
+            this.Close(); // Close the login form after the main form is closed
+        }
+        private void AddName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
